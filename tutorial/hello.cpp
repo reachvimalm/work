d5e330677e06a6c57023528128c898d1c998c239
vimal <reachvimalm@yahoo.com>
2018-08-05 08:06:47 -0400

    added in master 2

--- tutorial/hello.cpp
+++ tutorial/hello.cpp
@@ -5,5 +5,6 @@ std::cout<<"Hello world\n";
 std::cout<<"added on reachvimalm\n";
 std::cout<<"added in master branch 1\n";
 std::cout<<"added in c++ basic 1\n";
+std::cout<<"added in master branch 2\n";
 return 0;
 }
